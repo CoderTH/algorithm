442f455b6fdb98434b81159e61a4ab67d0738beb
CoderTh <CoderTh@outlook.com>
2021-01-02 20:09:19 +0800

    SelectionSort update

---
+++ SelectIonSort/ArrayGenerator.go
@@ -0,0 +1,24 @@
+package SelectIonSort
+
+import (
+	"math/rand"
+	"time"
+)
+
+//数组生成器
+func GenerateOrderedArray(n int)[]int  {
+	arr := make([]int,n,2*n)
+	for i:=0;i<len(arr);i++ {
+		arr[i] = i
+	}
+	return arr
+}
+
+func GenerateRandomArray(n int,bound int) []int {
+	arr := make([]int,0,0)
+	rand.Seed(time.Now().Unix())
+	for i:=0;i<n;i++ {
+		arr = append(arr, rand.Intn(bound))
+	}
+	return arr
+}
--- SelectIonSort/SelectionSort_test.go
+++ SelectIonSort/SelectionSort_test.go
@@ -1,10 +1,11 @@
 package SelectIonSort
 
 import (
+	"errors"
 	"fmt"
 	"testing"
+	"time"
 )
-
 
 func TestSelectionSort(t *testing.T) {
 	arr := []int{1,4,2,3,6,5}
@@ -13,4 +14,27 @@ func TestSelectionSort(t *testing.T) {
 		fmt.Print(v," ")
 	}
 	fmt.Println()
+}
+
+func TestGenerateSort(t *testing.T) {
+	arr := GenerateRandomArray(10000,10000)
+	start:= time.Now()
+	SelectionSort(arr)
+	elapsed := time.Since(start)
+	fmt.Println(" SelectionSort time of use：", elapsed)
+	err := CheckSort(arr)
+	if err != nil {
+		t.Error(err)
+	}else {
+		fmt.Println("Sort check pass")
+	}
+}
+
+func CheckSort(arr []int) error {
+	for i:=0;i<len(arr)-1;i++{
+		if arr[i]>arr[i+1] {
+			return errors.New("this sort is error ")
+		}
+	}
+	return nil
 }
