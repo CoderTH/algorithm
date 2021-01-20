7e31ba359ed791c57274171f292ef7e3f489f8cd
CoderTh <CoderTh@outlook.com>
2021-01-20 15:52:48 +0800

    InsertionSort bug fix

--- InsertionSort/insertionSort.go
+++ InsertionSort/insertionSort.go
@@ -1,14 +1,12 @@
 package InsertionSort
+
 //如果数组为有序的话，他的复杂度会变成O(n)
 //插入排序优化
 func InsertionSort(arr []int)  {
-	for i:=0;i<len(arr);i++{
-		tamp:=arr[i]
-		var j int
-		for j=i; j -1>=0&&arr[j]<arr[j-1];j--{
-				arr[j-1] = arr[j]
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0&&arr[j] < arr[j-1]; j-- {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
 		}
-		arr[j] = tamp
 	}
 }
 
--- MergeSort/test/MergeSort_test.go
+++ MergeSort/test/MergeSort_test.go
@@ -9,5 +9,10 @@ import (
 func TestMergeSort(t *testing.T)  {
 	n := 100000
 	array := SelectIonSort.GenerateRandomArray(n, n)
+	array2 := SelectIonSort.GenerateRandomArray(n, n)
+	array3 := SelectIonSort.GenerateRandomArray(n, n)
+	SortingHelper.SortTest("InsertionSort",array3,t)
+	SortingHelper.SortTest("SelectionSort",array2,t)
 	SortingHelper.SortTest("MergeSort",array,t)
+
 }
